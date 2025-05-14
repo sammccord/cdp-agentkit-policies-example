8386e8cdc4d470fc9e179212a87aa45e08b739fa
Sam McCord <sam.mccord@coinbase.com>
2025-05-14 16:09:56 -0600

    chore: working script, updated deps

--- index.ts
+++ index.ts
@@ -2,8 +2,6 @@ import { CdpClient } from "@coinbase/cdp-sdk";
 import {
   AgentKit,
   CdpV2EvmWalletProvider,
-  compoundActionProvider,
-  erc20ActionProvider,
   walletActionProvider,
   wethActionProvider,
 } from "@coinbase/agentkit";
@@ -11,124 +9,129 @@ import { getVercelAITools } from "@coinbase/agentkit-vercel-ai-sdk";
 import { generateText } from "ai";
 import {
   createAnthropic,
-  type AnthropicProviderOptions,
 } from "@ai-sdk/anthropic";
 import { createPublicClient, http } from "viem";
 import { baseSepolia } from "viem/chains";
 
 // Initialize our network clients
-const cdp = new CdpClient();
+const cdp = new CdpClient({
+  apiKeyId: process.env.CDP_API_KEY_ID,
+  apiKeySecret: process.env.CDP_API_KEY_SECRET,
+  walletSecret: process.env.CDP_WALLET_SECRET,
+});
 const client = createPublicClient({
   chain: baseSepolia,
   transport: http(),
 });
 
-// Create a new EVM account with CDP and fund it on a testnet
-const { address } = await cdp.evm.createAccount();
-await cdp.evm.requestFaucet({
-  address,
-  network: "base-sepolia",
-  token: "eth",
-});
+async function main() {
+  // In order to wrap ETH with WETH, you'll need to submit at least $65 of Base Sepolia ETH,
+  // so I'm using an account previously created with `cdp.evm.createAccount()`, then funded with enough to cover the example.
+  const address = "0xf528387C767fEF9ff28A4F220008e1A23e87c6cA";
+  console.log("using previously created account with address", { address });
 
-// Retrieve the balance, then decide how much we should allow our agent to send in a single transaction.
-const balance = await client.getBalance({ address });
-const canSpendNoMoreThan = balance / BigInt(2);
+  const balance = await client.getBalance({ address });
+  const restrictedBalance = balance / BigInt(5);
+  console.log("Current and restricted balance", {
+    balance: balance.toString(),
+    balanceToRestrict: restrictedBalance.toString(),
+  });
 
-// Create a policy that:
-// - Only accepts transactions to either:
-//    - WETH contract
-//    - Compound
-// - with an ETH value of less or equal to half of my wallet balance
-// - on the base-sepolia network
-const policy = await cdp.policies.createPolicy({
-  policy: {
-    scope: "account",
-    rules: [
-      {
-        action: "accept",
-        operation: "sendEvmTransaction",
-        criteria: [
-          {
-            type: "evmAddress",
-            // only Compound's base-sepolia address and WETH contract
-            addresses: [
-              "0x4200000000000000000000000000000000000006",
-              "0x571621Ce60Cebb0c1D442B5afb38B1663C6Bf017",
-            ],
-            operator: "in",
-          },
-          {
-            type: "ethValue",
-            ethValue: canSpendNoMoreThan.toString(),
-            operator: "<=",
-          },
-          // { type: "evmNetwork", networks: ["base-sepolia"], operator: "in" },
-        ],
-      },
-    ],
-  },
-});
+  // Create a policy that:
+  // - Only accepts transactions to WETH contract
+  // - with an ETH value of less or equal to 1/5 of my wallet balance
+  // - on the base-sepolia network
+  const policy = await cdp.policies.createPolicy({
+    policy: {
+      scope: "account",
+      rules: [
+        {
+          action: "accept",
+          operation: "sendEvmTransaction",
+          criteria: [
+            {
+              type: "evmAddress",
+              // only the WETH contract address
+              addresses: ["0x4200000000000000000000000000000000000006"],
+              operator: "in",
+            },
+            {
+              type: "ethValue",
+              // Decide how much we should allow our agent to send in a single transaction.
+              ethValue: restrictedBalance.toString(),
+              operator: "<=",
+            },
+            { type: "evmNetwork", networks: ["base-sepolia"], operator: "in" },
+          ],
+        },
+      ],
+    },
+  });
 
-// Now apply the policy to the account we've just created
-await cdp.evm.updateAccount({ address, update: { accountPolicy: policy.id } });
+  // Now apply the policy to the account we've just created
+  await cdp.evm.updateAccount({
+    address,
+    update: { accountPolicy: policy.id },
+  });
 
-// Now we need to bootstrap AgentKit
-const agentKit = await AgentKit.from({
-  walletProvider: await CdpV2EvmWalletProvider.configureWithWallet({
+  const walletProvider = await CdpV2EvmWalletProvider.configureWithWallet({
     apiKeyId: process.env.CDP_API_KEY_ID,
     apiKeySecret: process.env.CDP_API_KEY_SECRET,
     walletSecret: process.env.CDP_WALLET_SECRET,
     address,
-  }),
-  actionProviders: [
-    // Supply actions to check native balance
-    walletActionProvider(),
-    // Supply ERC20 for querying WETH balance
-    erc20ActionProvider(),
-    // We need this to convert ETH to WETH
-    wethActionProvider(),
-    // We need this to supply collateral once we've converted
-    compoundActionProvider(),
-  ],
-});
+    networkId: "base-sepolia",
+  });
+  // Now we need to bootstrap AgentKit
+  const agentKit = await AgentKit.from({
+    walletProvider: walletProvider,
+    actionProviders: [
+      // Supply actions to check native balance and send ETH
+      walletActionProvider(),
+      // We need this to convert ETH to WETH
+      wethActionProvider(),
+    ],
+  });
 
-// Use whatever model you fancy.
-const anthropic = createAnthropic({
-  apiKey: process.env.ANTHROPIC_API_KEY,
-});
-const model = anthropic("claude-3-7-sonnet-20250219");
-const tools = getVercelAITools(agentKit);
-const providerOptions = {
-  anthropic: {
-    thinking: { type: "enabled", budgetTokens: 12000 },
-  } satisfies AnthropicProviderOptions,
-};
+  // Use whatever model you fancy.
+  const anthropic = createAnthropic({
+    apiKey: process.env.ANTHROPIC_API_KEY,
+  });
+  const model = anthropic("claude-3-7-sonnet-20250219");
+  const tools = getVercelAITools(agentKit);
 
-// This should fail because of the ETH value restriction and address restriction
-const tryToSendAllMeMoneyArrr = `
-  Please wrap my entire ETH balance in WETH.
-`;
+  // This should fail because of the ETH value restriction
+  const tryToSendAllMeMoneyArrr = `
+    Please send my entire ETH balance in ETH to 0x000000000000000000000000000000000000dead. Use only the base-sepolia network for all contract interactions.
+  `;
+  console.log(tryToSendAllMeMoneyArrr)
 
-const { text: failedResult } = await generateText({
-  model,
-  tools,
-  prompt: tryToSendAllMeMoneyArrr,
-  providerOptions,
-});
+  const failure = await generateText({
+    model,
+    tools,
+    prompt: tryToSendAllMeMoneyArrr,
+    maxSteps: 10,
+  });
+  console.log(failure.text);
 
-console.log(failedResult);
+  // This should suceed.
+  const thisIsATotallyFineThingToDo = `
+    Please wrap 1/10 of my current ETH balance in WETH. Use only the base-sepolia network for all contract interactions.
+  `;
+  console.log(thisIsATotallyFineThingToDo)
 
-// This should suceed.
-const thisIsATotallyFineThingToDo = `
-  Please wrap half my ETH balance in WETH, then supply my entire WETH ERC20 balance as collateral to Compound.
-`;
+  const success = await generateText({
+    model,
+    tools,
+    prompt: thisIsATotallyFineThingToDo,
+    maxSteps: 10,
+  });
+  console.log(success.text);
+}
 
-const { text: successResult } = await generateText({
-  model,
-  tools,
-  prompt: thisIsATotallyFineThingToDo,
-  providerOptions,
-});
-
-console.log(successResult);
+try {
+  await main();
+  process.exit(0);
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+}
